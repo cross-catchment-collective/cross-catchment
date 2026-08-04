7791b96526d07942bbcf7ec83f483976453b8499
junior masete <juniormasete0@gmail.com>
2026-08-03 17:15:10 -0700

    fixing base

--- frontend/cross-catchment-collective/src/App.jsx
+++ frontend/cross-catchment-collective/src/App.jsx
@@ -22,7 +22,7 @@ function App() {
 
   return (
     <>
-      <BrowserRouter basename="/cross-catchment-collective">
+      <BrowserRouter basename="/">
         <Navbar />
         <Routes>
           <Route path="/" element={<Homepage />} />
